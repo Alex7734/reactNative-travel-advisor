e23c4f2c49a8d2c007c62956cae18d9f3a47dea8
Alex <alexmihoc@yahoo.com>
2023-07-06 14:20:24 +0300

    feature: added detail view of restaurant

--- src/components/ItemsCardContainer.jsx
+++ src/components/ItemsCardContainer.jsx
@@ -1,10 +1,17 @@
 import { FontAwesome } from "@expo/vector-icons";
 import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-function ItemsCardContainer({ imageSrc, title, location }) {
+function ItemsCardContainer({ imageSrc, title, location, data }) {
+  
+  const navigation = useNavigation();
+  
   return (
-    <TouchableOpacity className=" w-[45%] rounded-sm border border-gray-300 space-y-2 px-3 py-2">
+    <TouchableOpacity
+     onPress={() => navigation.navigate("ItemScreen", {param: data})} 
+     className="w-[45%] rounded-sm border border-gray-300 space-y-2 px-3 py-2"
+    >
       <Image
         source={{ uri: imageSrc }}
         className="w-full h-40 rounded-md object-cover"
